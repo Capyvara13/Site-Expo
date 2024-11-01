42f695d2b1f8b8558e3e19f3735541b6b1f1bb10
Capyvara13 <tiagorafleite@gmail.com>
2024-11-01 11:38:07 -0300

    fazendo a responsividade do menu mobile

--- navBar/navBarScript.js
+++ navBar/navBarScript.js
@@ -8,12 +8,52 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.querySelector('nav').appendChild(div);
                 //Código do navBar
                 //Variaveis para menu Mobile
-                let menuButton = document.getElementById("mobileMenuButton");
+                let menuButton = document.getElementById("burger");
                 let menuBar1 = document.getElementById("bar1");
                 let menuBar2 = document.getElementById("bar2");
                 let menuBar3 = document.getElementById("bar3");
                 //Código do menu
-                
+                let isMenuButtonClicked = false;
+                function menuMobile() {
+                    isMenuButtonClicked = !isMenuButtonClicked;
+                    if (isMenuButtonClicked) {
+                        //Animação do menu
+                        menuBar1.classList.remove("animationBar1Out");
+                        menuBar2.classList.remove("animationBar2Out");
+                        menuBar3.classList.remove("animationBar3Out");
+                        menuBar1.classList.add("animationBar1In");
+                        menuBar2.classList.add("animationBar2In");
+                        menuBar3.classList.add("animationBar3In");
+
+                        menuBar3.style.top = "5.8vh";
+                        menuBar3.style.transform = "rotate(45deg)";
+                        menuBar1.style.top = "5.8vh";
+                        menuBar1.style.transform = "rotate(-45deg)";
+                        menuBar2.style.width = "0vw";
+                        //--animação do menu--
+
+                        //Guia de menu
+                    } else {
+                        //Animação do menu
+                        menuBar1.classList.remove("animationBar1In");
+                        menuBar2.classList.remove("animationBar2In");
+                        menuBar3.classList.remove("animationBar3In");
+                        menuBar1.classList.add("animationBar1Out");
+                        menuBar2.classList.add("animationBar2Out");
+                        menuBar3.classList.add("animationBar3Out");
+
+                        menuBar3.style.top = "8vh";
+                        menuBar3.style.transform = "rotate(0deg)";
+                        menuBar1.style.top = "4vh";
+                        menuBar1.style.transform = "rotate(0deg)";
+                        menuBar2.style.width = "15vw";
+                        //--animação do menu--
+
+                        //Guia de menu
+                    }
+                };
+
+                menuButton.addEventListener("click", menuMobile);
             } else {
                 console.error('Elemento #IDdoElemento não encontrado no HTML carregado');
             }
